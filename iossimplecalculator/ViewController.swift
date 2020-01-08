d6b78eba9159eb6e94e15f97d6d572d03e6b5683
Chowdhury Asif Uddin Ahmed <asif000473@gmail.com>
2020-01-08 04:27:22 -0800

    Initial Commit

---
+++ iossimplecalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iossimplecalculator
+//
+//  Created by administrator on 1/8/20.
+//  Copyright © 2020 administrator. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
